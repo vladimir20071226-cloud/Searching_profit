2ceafcc1f8c7807074fb5fc98f08ea0e9f253bc4
vladimir20071226-cloud <vladimir20071226@gmail.com>
2026-05-24 12:17:59 +0500

    Update decorators.py

--- product/decorators.py
+++ product/decorators.py
@@ -4,7 +4,7 @@ def verified_required(func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect ("login")
-        if not request.user.email_verified:
+        if not request.user.profile.email_verified:
             messages.warning(request, "Подтвердите email, чтобы продолжить")
             return redirect("verify_email")
         return func(request, *args, **kwargs)
